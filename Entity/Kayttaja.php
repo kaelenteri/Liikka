d89788131fda5ff4f61e57f6707a76b28acbbc2e
Make Makke <kaelenteri@gmail.com>
2013-07-05 23:19:19 +0300

    käyttäjät ja login lisätty

--- Entity/Kayttaja.php
+++ Entity/Kayttaja.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Liikka\Entity;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,59 +13,126 @@
  * @author Make
  */
 class Kayttaja {
+
+    /**
+     *
+     * @var string $kayttajanimi
+     */
     private $kayttajanimi;
+
+    /**
+     *
+     * @var string $salasana 
+     */
     private $salasana;
+
+    /**
+     *
+     * @var string $etunimi 
+     */
     private $etunimi;
+
+    /**
+     *
+     * @var string $sukunimi 
+     */
     private $sukunimi;
+
+    /**
+     *
+     * @var double $pituus
+     */
     private $pituus;
-    
 
-
+    /**
+     * 
+     * @return string
+     */
     public function getSalasana() {
         return $this->salasana;
     }
 
+    /**
+     * 
+     * @param string $salasana
+     * @return \Liikka\Entity\Kayttaja
+     */
     public function setSalasana($salasana) {
         $this->salasana = $salasana;
         return $this;
     }
 
-        
+    /**
+     * 
+     * @return string
+     */
     public function getEtunimi() {
         return $this->etunimi;
     }
 
+    /**
+     * 
+     * @param string $etunimi
+     * @return \Liikka\Entity\Kayttaja
+     */
     public function setEtunimi($etunimi) {
         $this->etunimi = $etunimi;
         return $this;
     }
 
+    /**
+     * 
+     * @return string
+     */
     public function getSukunimi() {
         return $this->sukunimi;
     }
 
+    /**
+     * 
+     * @param string $sukunimi
+     * @return \Liikka\Entity\Kayttaja
+     */
     public function setSukunimi($sukunimi) {
         $this->sukunimi = $sukunimi;
         return $this;
     }
 
-
+    /**
+     * 
+     * @return string
+     */
     public function getKayttajanimi() {
         return $this->kayttajanimi;
     }
 
+    /**
+     * 
+     * @param string $kayttajanimi
+     * @return \Liikka\Entity\Kayttaja
+     */
     public function setKayttajanimi($kayttajanimi) {
         $this->kayttajanimi = $kayttajanimi;
+        return $this;
     }
 
+    /**
+     * 
+     * @return double
+     */
     public function getPituus() {
         return $this->pituus;
     }
 
+    /**
+     * 
+     * @param double $pituus
+     * @return \Liikka\Entity\Kayttaja
+     */
     public function setPituus($pituus) {
         $this->pituus = $pituus;
+        return $this;
     }
-
 
 }
 
---
+++ Entity/Kayttajat.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Liikka\Entity;
+
+use Liikka\Entity\Kayttaja;
+use Liikka\Entity\ApuMetodit;
+
+include_once 'Kayttaja.php';
+include_once 'ApuMetodit.php';
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Kayttajat
+ *
+ * @author Make
+ */
+class Kayttajat {
+
+    /**
+     *
+     * @var Kayttaja[] 
+     */
+    private $kayttajat;
+
+    /**
+     * 
+     * @return Kayttaja[]
+     */
+    public function getKayttajat() {
+        return $this->kayttajat;
+    }
+
+    /**
+     * 
+     * @param array $kayttajat
+     * @return \Liikka\Entity\Kayttajat
+     */
+    public function setKayttajat(array $kayttajat) {
+        $this->kayttajat = $kayttajat;
+        return $this;
+    }
+
+    /**
+     * Etsitään löytyykö käyttänimeä vastaava käyttäjänimi tietokannasta. 
+     * Palautetaan käyttäjä-olio. Jos ei löydy niin palautetaan null.
+     * @param string $kayttajanimi
+     * @return \Liikka\Entity\Kayttaja|null 
+     */
+    public function etsi($kayttajanimi) {
+
+        $kayttajanimi = ApuMetodit::muunnaJonoKyselyaVarten($kayttajanimi);
+        $conn = mysqli_connect('localhost', 'make', 'toppi', 'liikka', '3306');
+        if (!$conn) {
+            die('Could not connect to MySQL: ' . mysqli_connect_error());
+        }
+
+        $kysely = "SELECT * kayttaja WHERE kayttajanimi = $kayttajanimi";
+        $tulos = mysqli_query($conn, $kysely);
+
+        if (mysqli_num_rows($tulos)) {
+            $rivi = $tulos->fetch_array(MYSQLI_ASSOC);
+
+            $kayttaja = new Kayttaja($rivi['kayttajanimi'], $rivi['salasana'], $rivi['etunimi'], $rivi['sukunimi'], $rivi['pituus']);
+            mysqli_close($conn);
+            return $kayttaja;
+        }
+        mysqli_close($conn);
+        return NULL;
+    }
+
+    /**
+     * 
+     * @param string $kayttajanimi
+     * @param string $salasana
+     * @return boolean
+     */
+    public function kirjautuminenOk($kayttajanimi, $salasana) {
+        /* @var $kayttaja Kayttaja */
+        $kayttaja = etsi($kayttajanimi);
+        if ($kayttaja != NULL) {
+            if ($kayttaja->getSalasana() === $salasana) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+?>
